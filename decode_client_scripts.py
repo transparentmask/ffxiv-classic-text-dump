f2b2c0b19e5d1302875b9c3528c9f5d3af74b032
Martin Yin <transparentmask@gmail.com>
2019-08-01 15:59:11 +0800

    decode script file name. Thanks oott123

--- decode_client_scripts.py
+++ decode_client_scripts.py
@@ -19,16 +19,23 @@ MAGIC_HEADER_NO_XOR_LEN = len(MAGIC_HEADER_NO_XOR)
 MAGIC_HEADER_SAN = b'\x73\x61\x6E\x65'
 MAGIC_HEADER_SAN_LEN = len(MAGIC_HEADER_SAN)
 
+NAME_SEQ = list("_abcdefghijklmnopqrstuvwxyz0123456789_")
+NAME_SEQ_REV = list(reversed(NAME_SEQ))
+
+
+def decode_file_name(name):
+    return ''.join([NAME_SEQ_REV[NAME_SEQ.index(char)] for char in list(name)])
+
 
 def call_unlua(contents):
     temp = tempfile.NamedTemporaryFile()
     temp.write(contents)
     temp.flush()
-    with open('../temp', 'wb') as f:
-        f.write(contents)
+
     proc = Popen(['java', '-jar', UNLUA_JAR_PATH, temp.name], stdout=PIPE)
     decoded = proc.stdout.read()
     temp.close()
+
     if len(decoded) == 0:
         raise Exception
     return decoded
@@ -71,25 +78,27 @@ def decode(file_path, target_path):
                 f.write(decoded)
         else:
             print('not correct file: %s' % file_path)
-    except Exception as e:
+    except Exception:
         print('decode failed: %s' % file_path)
-        exit(0)
 
 
 def main():
     list_dirs = os.walk(BASE_DATA_PATH)
     for root, dirs, files in list_dirs:
-        target_root = os.path.join(EXPORT_PATH, root.replace(BASE_DATA_PATH, '', 1).strip('/'))
+        sub_root = root.replace(BASE_DATA_PATH, '', 1).strip(os.sep).split(os.sep)
+        target_root = os.path.join(EXPORT_PATH, os.sep.join([decode_file_name(s) for s in sub_root]))
         try:
             os.makedirs(target_root)
         except Exception:
             pass
         for file in files:
             file_path = os.path.join(root, file)
+            file = '%s.%s' % (decode_file_name(file.split('.')[0]), 'san' if file.endswith('san') else 'lua')
             target_path = os.path.join(target_root, file)
             decode(file_path, target_path)
 
 
 if __name__ == '__main__':
+    # print(decode_file_name('rq9qpr89r57y9rr'))
     # decode('../FINAL FANTASY XIV/client/script/0p635/0p63589r57y9rr.le.lpb', '')
     main()
